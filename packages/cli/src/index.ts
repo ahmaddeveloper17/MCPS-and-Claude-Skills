11df8d6bf2ce626751d95020c161c4b55fa18b70
Ahmad Mujtaba <ahmaddeveloper174@gmail.com>
2026-07-05 17:11:48 +0500

    cli shared core

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,12 +1,6 @@
 import { Command } from "commander";
+import { sendTelegramMessage } from "sendkit-core";
 
-type TelegramResponse = {
-  ok: boolean;
-  result?: {
-    message_id?: number;
-  };
-  description?: string;
-};
 const program = new Command();
 
 program
@@ -17,9 +11,31 @@ program
   .argument("<chatId>", "Telegram Chat ID")
   .argument("<message>", "Message test to send")
   .action(async (chatId: string, message: string) => {
-    console.log("ChatId", chatId);
-    console.log("message", message);
-    process.exit(1);
+    try {
+      const token = process.env.TELEGRAM_BOT_TOKEN;
+      if (!token) {
+        console.error(
+          "Error: TELEGRAM_BOT_TOKEN environment variable is not set.",
+        );
+        process.exit(1);
+      }
+      if (!chatId || !message) {
+        console.error("Error: Both chatId and message are required.");
+        process.exit(1);
+      }
+
+      const result = await sendTelegramMessage({
+        botToken: token,
+        chatId,
+        message,
+      });
+
+      console.log(
+        `Message has been send to ${result.chatId} and text ${result.messageId}`,
+      );
+    } catch (error) {
+      console.error("Error sending Telegram message:", error);
+    }
   });
 
 program.parseAsync(process.argv);
